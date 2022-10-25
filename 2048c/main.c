85b1c96e30802eab0e60eedec64de6b4fede7518
lm3267 <lm3267@drexel.edu>
2022-10-26 00:16:11 +0300

    changed struct Tile to just integers

--- 2048c/main.c
+++ 2048c/main.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 
-typedef struct {
-	int row;
-	int col;
-	int value;
-} tile;
-
-tile** field;
+int** field;
 int side = 4;
 
 void getFieldSize() {	//Get user input for the side sizes
@@ -15,20 +9,16 @@ void getFieldSize() {	//Get user input for the side sizes
 }
 
 void generateField() {		//Creates the initial field
-	tile t;
-	t.value = 0;
-	field = (tile**)malloc(side * sizeof(tile*));
+	field = (int**)malloc(side * sizeof(int*));
 	for (int i = 0; i < side; ++i) {
-		field[i] = (tile*)malloc(side * sizeof(tile));
+		field[i] = (int*)malloc(side * sizeof(int));
 		for (int j = 0; j < side; ++j) {
-			t.row = i;
-			t.col = j;
-			t.value = 0;
-			if (i == 1 && j == 1) t.value = 1;
-			if (i == 3 && j == 1) t.value = 2;
-			if (i == 3 && j == 3) t.value = 1;
 
-			field[i][j] = t;
+			field[i][j] = 0;
+			if (i == 1 && j == 1) field[i][j] = 1;
+			if (i == 3 && j == 1) field[i][j] = 2;
+			if (i == 3 && j == 3) field[i][j] = 1;
+
 		}
 	}
 }
@@ -36,7 +26,7 @@ void generateField() {		//Creates the initial field
 void printField() {		//Output the field
 	for (int i = 0; i < side; ++i) {
 		for (int j = 0; j < side; ++j) {
-			printf("%d ", field[i][j].value);
+			printf("%d ", field[i][j]);
 		}
 		printf("\n");
 	}
@@ -46,7 +36,7 @@ void printField() {		//Output the field
 void rotateField() {		//Rotate field left 90 degrees (so we only slide to one side)
 	for (int i = 0; i < side / 2; ++i) {
 		for (int j = i; j < side - i - 1; ++j) {
-			tile t = field[i][j];
+			int t = field[i][j];
 			field[i][j] = field[j][side - 1 - i];
 			field[j][side - 1 - i] = field[side - 1 - i][side - 1 - j];
 			field[side - 1 - i][side - 1 - j] = field[side - 1 - j][i];
@@ -71,12 +61,12 @@ void slideField(char direction) {		//Slide field in a given direction
 	for (int i = 0; i < rotation; ++i) {
 		rotateField();
 	}
-	printf("rotated\n");
-	printField();
 	for (int i = 0; i < side; ++i) {
 		for (int j = 1; j < side; ++j) {
-			if (field[i][j].value)
-				if (checkSpot(field[i][j])) field[i][j].value = 0;
+			if (field[i][j] && checkSpot(i, j)) {
+				field[i][j] = 0;
+				//printField();
+			}
 		}
 	}
 	if (rotation > 0) {		//If we rotated the field - return to its normal position
@@ -86,19 +76,17 @@ void slideField(char direction) {		//Slide field in a given direction
 	}
 }
 
-int checkSpot(tile t) {		//Check where a tile goes to (true if moved, false if stayed)
-	int row = t.row;
-	for (int i = 0; i < t.col; ++i) {
-		if (field[i][row].value == 0) {
-			field[i][row].value = t.value;
-			printf("1 %d\n", i);
-			printField();
+int checkSpot(int row, int col) {		//Check where a tile goes to (true if moved, false if stayed)
+	int value = field[row][col];
+	for (int i = 0; i < col; ++i) {
+		if (field[row][i] == 0) {
+			field[row][i] = value;
+			//printf("1 %d\n", i);
 			return 1;
 		}
-		else if (field[row][i].value == t.value) {
-			field[row][i].value *= 2;
-			printf("2\n");
-			printField();
+		else if (field[row][i] == value) {
+			field[row][i] *= 2;
+			//printf("2\n");
 			return 1;
 		}
 	}
@@ -113,6 +101,10 @@ int main() {
 	//printField();
 	//rotateField();
 	slideField('r');
-	//printField();
+	printField();
+	slideField('u');
+	printField();
+	slideField('r');
+	printField();
 	return 0;
 }
