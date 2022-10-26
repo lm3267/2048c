130ddbacb79aaa638da5c2a00bf7311b69b69c56
lm3267 <lm3267@drexel.edu>
2022-10-26 21:10:33 +0300

    final

--- 2048c/main.c
+++ 2048c/main.c
@@ -1,32 +1,86 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
+#include <string.h>
 
 int** field;
-int side = 4;
+int** fieldCopy;
+int side = 0;
+int max = 2;
 
 void getFieldSize() {	//Get user input for the side sizes
 	printf("Enter side size (more than 2)\n");
-	scanf_s("%d", &side);
+	while (side <= 2) {
+		scanf_s("%d", &side);
+		if (side <= 2) printf("Invalid value\n");
+	}
 }
 
-void generateField() {		//Creates the initial field
-	field = (int**)malloc(side * sizeof(int*));
-	for (int i = 0; i < side; ++i) {
-		field[i] = (int*)malloc(side * sizeof(int));
-		for (int j = 0; j < side; ++j) {
-
-			field[i][j] = 0;
-			if (i == 1 && j == 1) field[i][j] = 1;
-			if (i == 3 && j == 1) field[i][j] = 2;
-			if (i == 3 && j == 3) field[i][j] = 1;
-
+void generateRandom(int needRand) {		//Generates 2 or 4 in a random empty spot on the field
+	int value = 2;
+	if (needRand) {
+		int twoOrFour = rand() % 100;
+		value = twoOrFour > 80 ? 4 : 2;
+	}
+	int foundEmpty = 0;
+	while (!foundEmpty) {
+		int row = rand() % side;
+		int col = rand() % side;
+		if (!field[row][col]) {
+			field[row][col] = value;
+			foundEmpty = 1;
 		}
 	}
 }
 
+void generateField() {		//Creates the initial field
+	field = (int**)malloc(side * sizeof(int*));
+	fieldCopy = (int**)malloc(side * sizeof(int*));;
+	for (int i = 0; i < side; ++i) {
+		field[i] = (int*)malloc(side * sizeof(int));
+		fieldCopy[i] = (int*)malloc(side * sizeof(int));
+		for (int j = 0; j < side; ++j) {
+			field[i][j] = 0;
+			//if (i == 0 && j == 3) field[i][j] = 4;
+			//if (i == 1 && j == 3) field[i][j] = 2;
+			//if (i == 2 && j == 3) field[i][j] = 4;
+			//if (i == 3 && j == 3) field[i][j] = 2;
+		}
+	}
+	generateRandom(0);
+}
+
+int countDigits(int value) {
+	int digits = 0;
+	while (value > 0) {
+		value /= 10;
+		++digits;
+	}
+	return digits;
+}
+
 void printField() {		//Output the field
+	system("cls");
 	for (int i = 0; i < side; ++i) {
 		for (int j = 0; j < side; ++j) {
-			printf("%d ", field[i][j]);
+			int digits = countDigits(field[i][j]);
+			switch (digits) {
+				case 0:
+				case 1: 
+					printf("  %d |", field[i][j]);
+					break;
+				case 2: 
+					printf(" %d |", field[i][j]);
+					break;
+				case 3: 
+					printf(" %d|", field[i][j]);
+					break;
+				case 4:
+					printf("%d|", field[i][j]);
+					break;
+				}
+			//printf("%d ", field[i][j]);
 		}
 		printf("\n");
 	}
@@ -45,7 +99,7 @@ void rotateField() {		//Rotate field left 90 degrees (so we only slide to one si
 	}
 }
 
-void slideField(char direction) {		//Slide field in a given direction
+int slideField(char direction) {		//Slide field in a given direction
 	int rotation = 0;
 	switch (direction) {
 		case 'u': 
@@ -61,11 +115,12 @@ void slideField(char direction) {		//Slide field in a given direction
 	for (int i = 0; i < rotation; ++i) {
 		rotateField();
 	}
+	int hasChange = 0;		//Check if the field changes with this rotation or no;
 	for (int i = 0; i < side; ++i) {
 		for (int j = 1; j < side; ++j) {
 			if (field[i][j] && checkSpot(i, j)) {
 				field[i][j] = 0;
-				//printField();
+				hasChange = 1;
 			}
 		}
 	}
@@ -74,37 +129,97 @@ void slideField(char direction) {		//Slide field in a given direction
 			rotateField();
 		}
 	}
+	return hasChange;
 }
 
 int checkSpot(int row, int col) {		//Check where a tile goes to (true if moved, false if stayed)
 	int value = field[row][col];
-	for (int i = 0; i < col; ++i) {
-		if (field[row][i] == 0) {
-			field[row][i] = value;
-			//printf("1 %d\n", i);
-			return 1;
+	int cellToGo = col;
+	for (int i = col - 1; i >= 0; --i) {
+		if (field[row][i] != 0) {
+			if (field[row][i] == value) {
+				field[row][i] *= 2;
+				if (field[row][i] > max) max = field[row][i];
+				return 1;
+			}
+			else if (cellToGo != col) {
+				field[row][cellToGo] = value;
+				return 1;
+			}
+			else return 0;
 		}
-		else if (field[row][i] == value) {
-			field[row][i] *= 2;
-			//printf("2\n");
-			return 1;
-		}
+		else cellToGo = i;
+	}
+	if (!cellToGo) {		//when cellToGo is 0;
+		field[row][0] = value;
+		return 1;
 	}
 	return 0;
 }
 
+int countEmpty() {
+	int cnt = 0;
+	for (int i = 0; i < side; ++i) {
+		for (int j = 0; j < side; ++j) {
+			if (field[i][j] == 0) ++cnt;
+		}
+	}
+	return cnt;
+}
+
+char* checkEndGame() {
+	if (max == 2048) return "YOU WON!";
+	int emptySpaces = countEmpty();
+	if (emptySpaces == 0) {
+		int** fieldCopy = field;
+		if (!slideField('l') && !slideField('r') && !slideField('u') && !slideField('d')) return "YOU LOST!";
+		field = fieldCopy;
+	}
+	return "Continue";
+}
+
+char getSlideDirection() {
+	char inp = _getch();
+	if (inp == 'w') return 'u';
+	else if (inp == 's') return 'd';
+	else if (inp == 'd') return 'r';
+	else if (inp == 'a') return 'l';
+	printf("Invalid Input\n");
+	return 'e';		//Error - incorrect input
+}
+
+void copyArray(int toCopy) {
+	for (int i = 0; i < side; ++i) {
+		for (int j = 0; j < side; ++j) {
+			if (toCopy) fieldCopy[i][j] = field[i][j];
+			else field[i][j] = fieldCopy[i][j];
+		}
+	}
+}
+
 int main() {
-	//getFieldSize();
+	time_t t;
+	srand((unsigned)time(&t));
+	getFieldSize();
 	generateField();
+	char* end;
+	while (1) {		//break inside the loop
+		printField();
+		char direction = 'e';
+		while (direction == 'e') {
+			direction = getSlideDirection();
+			if (direction != 'e') {
+				copyArray(1);
+				if (!slideField(direction)) direction = 'e';
+				copyArray(0);
+			}
+		}
+		slideField(direction);
+		end = checkEndGame();
+		if (end != "Continue") break;
+		generateRandom(1);
+	}
 	printField();
-	//rotateField();
-	//printField();
-	//rotateField();
-	slideField('r');
-	printField();
-	slideField('u');
-	printField();
-	slideField('r');
-	printField();
+	printf(end);
 	return 0;
 }
